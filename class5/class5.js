3140f39c12c44fd6f2cc5074c07d643b6f40cafc
Prakash Das <opto3789@gmail.com>
2023-03-07 17:44:30 +0530

    class 1 complete

---
+++ class5/class5.js
@@ -0,0 +1,38 @@
+// create a variable of type string and try to add a number to it
+
+let a = "hello";
+let b = 3 ;
+
+console.log(a+b)
+
+// use typeof operator to find the datatype of thestring in last question
+
+console.log(typeof (a+b))
+
+// create a const object in javascript , can you change it to hold a number later 
+
+// ans is no 
+
+// how can you push something in a constent object 
+
+const a1 = {
+    name : "Prakash",
+    age : 22
+
+}
+
+a1["friend"]= "Rakesh"
+a1["name"]= "Anny"  // to change any predefine data in object
+console.log(a1)
+
+// whrite a js program to create a  word-meaning distinary of 5 words
+
+const dict = {
+   helo : "gjbfs aldfjaoijv adfkno",
+    kelo : "gjbadfs aldfbsdcvzxdvfjaoijv adfkno",
+    lolo : "gjbfs alddfvdzfvfjaoijv adfkno",
+    polo : "gjbfs aldfdvsdvffjaoijv adfkno"
+   
+}
+console.log(dict.helo)
+
